cc486f758541f738666e9c1bf79a0a9f3735b312
itamar <itamar@feezback.com>
2019-02-28 20:18:56 +0200

    Add simple stuff

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ class Slide:
     def __init__(self, photos: List[Photo]):
         self.photos = photos
 
-    @functools.lru_cache
     @property
     def tags(self):
-        return {p.tags for p in self.photos}
+        result = set()
+        for p in self.photos:
+            result.update(set(p.tags))
+        return result
 
 
 class Solution:
     def __init__(self, slides: List[Slide]):
         self.slides = slides
+
+    def calc_score(self) -> int:
+        return sum(calc_interest(self.slides[i], self.slides[i+1]) for i in range(
+            len(self.slides) - 1))
 
 
 def calc_interest(slide1: Slide, slide2: Slide) -> int:
@@ -40,7 +46,7 @@ def read_file(filename: str) -> List[Photo]:
         # num_of_photos = int(lines[0])
         photos = []
         for index, line in enumerate(lines[1:]):
-            orientation, num_of_tags, *tags = line.split(" ")
+            orientation, num_of_tags, *tags = line.strip().split(" ")
             photo = Photo(index, orientation, tags)
             photos.append(photo)
     return photos
@@ -48,13 +54,26 @@ def read_file(filename: str) -> List[Photo]:
 
 def write_solution(solution: Solution, filename: str):
     with open(filename, "w") as file:
-        file.write(str(len(solution.slides)))
+        file.write(str(len(solution.slides)) + "\n")
         for slide in solution.slides:
-            file.write(", ".join(s.index for s in slide.photos))
+            file.write(", ".join(str(s.index) for s in slide.photos) + "\n")
+
+
+def solve(photos: List[Photo]) -> Solution:
+    # temp test
+    slides = []
+    for photo in photos:
+        slides.append(Slide([photo]))
+    return Solution(slides)
 
 
 def main():
-    print("This code does nothing right now!")
+    filename = "a_example.txt"
+    photos = read_file(filename)
+    print(f"Calculating for {filename}…")
+    solution = solve(photos)
+    print(f"Score of solution: {solution.calc_score()}")
+    write_solution(solution, filename.replace(".txt", "_out.txt"))
 
 
 if __name__ == '__main__':
